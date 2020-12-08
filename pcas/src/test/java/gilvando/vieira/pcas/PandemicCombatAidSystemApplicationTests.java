df19d486df9798d40ebcdcff4f267ad6e4d783dd
Gilvando Lucas Vieira <gilvandolucasvieira@gmail.com>
2020-12-08 11:03:14 -0300

    Adicionando projeto java

---
+++ pcas/src/test/java/gilvando/vieira/pcas/PandemicCombatAidSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package gilvando.vieira.pcas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicCombatAidSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
